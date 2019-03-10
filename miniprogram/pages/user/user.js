1c73139f65aad7618a17a031c1e8103f17d1a0c4
AnCoSONG <1197252996@qq.com>
2019-03-10 20:28:14 +0900

    调整user页面

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -11,9 +11,8 @@ Page({
   data: {
     activeNames: ['1'],
 
-
+    recycle_records_label: '请先登录',
     tickets_label: '请先登录',
-    tickets_desc: '你没有一张卡券',
     point_desc: '请先登录',
 
     about_us_show: false,
@@ -22,10 +21,10 @@ Page({
       battery_point: 0,
       tea_point: 0,
       recycle_records: [],
-      tea_tickets: ['1'],
-      doll_tickets: ['2'],
+      tea_tickets: [],
+      doll_tickets: [],
       logged: false,
-      username: "请登录",
+      username: "请点头像登录",
       userInfo: {},
       avatarUrl: ""
 
@@ -36,37 +35,38 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                  point_desc: "继续加油哦",
-                  tickets_label: '继续加油哦',
-                  user: {
-                    battery_point: 50,
-                    tea_point: 50,
-                    recycle_records: ['one'],
-                    logged: true,
-                    username: res.userInfo.nickName,
-                    avatarUrl: res.userInfo.avatarUrl,
-                    userInfo: res.userInfo,
-                    tea_tickets: ['1', '2'],
-                    doll_tickets: ['1', '2'],
-                  }
-                }),
-                app.logged = true;
-              app.avaterUrl = res.userInfo.avatarUrl;
-              app.username = res.userInfo.nickName;
-              app.userInfo = res.userInfo;
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           this.setData({
+    //               point_desc: "继续加油哦",
+    //               tickets_label: '继续加油哦',
+    //               recycle_records_label: '继续加油哦',
+    //               user: {
+    //                 battery_point: 50,
+    //                 tea_point: 50,
+    //                 recycle_records: ['one'],
+    //                 logged: true,
+    //                 username: res.userInfo.nickName,
+    //                 avatarUrl: res.userInfo.avatarUrl,
+    //                 userInfo: res.userInfo,
+    //                 tea_tickets: ['1', '2'],
+    //                 doll_tickets: ['1', '2'],
+    //               }
+    //             }),
+    //             app.logged = true;
+    //           app.avaterUrl = res.userInfo.avatarUrl;
+    //           app.username = res.userInfo.nickName;
+    //           app.userInfo = res.userInfo;
 
-            }
-          })
-        }
-      }
-    })
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   },
 
   /**
@@ -126,17 +126,17 @@ Page({
             tea_point: 50,
             recycle_records: ['one'],
             logged: true,
-            username: e.userInfo.nickName,
-            avatarUrl: e.userInfo.avatarUrl,
-            userInfo: e.userInfo,
+            username: e.detail.userInfo.nickName,
+            avatarUrl: e.detail.userInfo.avatarUrl,
+            userInfo: e.detail.userInfo,
             tea_tickets: ['1', '2'],
             doll_tickets: ['1', '2'],
           }
         }),
         app.logged = true;
-      app.avaterUrl = res.userInfo.avatarUrl;
-      app.username = res.userInfo.nickName;
-      app.userInfo = res.userInfo;
+      app.avaterUrl = e.detail.userInfo.avatarUrl;
+      app.username = e.detail.userInfo.nickName;
+      app.userInfo = e.detail.userInfo;
     }
   },
 
@@ -162,6 +162,7 @@ Page({
       message: '这款小程序由中南大学“杂症听诊器”团队设计，四川大学某不愿意透露姓名的宋同学和曹同学研发。在这里，有神器的魔法——用掉的电池可以变成橱窗里的娃娃，喝光的奶茶杯可以变成满满的奶茶。欢迎来到神器的电池杯杯世界。'
     }).then(() => {
       // on close
+      console.log("close")
     });
   }
 })
