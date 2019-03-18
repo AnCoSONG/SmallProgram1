989e1dc9aa6749b595179f347f2ccbcf213121e4
AnCoSONG <1197252996@qq.com>
2019-03-18 15:13:41 +0900

    完善奶茶页

--- miniprogram/pages/copeTeaCup/copeTeaCup.js
+++ miniprogram/pages/copeTeaCup/copeTeaCup.js
@@ -1,5 +1,7 @@
 // miniprogram/pages/copeTeaCup/copeTeaCup.js
 
+import Toast from "../../dist/toast/toast";
+
 const rules = {
   YHT: '益禾堂',
   SYSXC: '书亦烧仙草',
@@ -23,7 +25,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUpLoadType:false
+    imgUpLoadType: '未选择',
+    teaCupCode: ''
   },
 
   /**
@@ -84,5 +87,92 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onSubmit(e) {
+    if (this.data.teaCupCode) {
+      if (this.data.imgUpLoadType != '未选择') {
+        //提交
+        Toast.loading({
+          mask: true,
+          duration: 3000, // 持续展示 toast
+          forbidClick: true, // 禁用背景点击
+          message: '正在上传',
+          selector: '#submit-toast'
+        })
+      } else {
+        console.log("没图")
+      }
+    } else {
+      console.log("没号")
+    }
+  },
+
+  onReset(e) {
+    this.setData({
+      teaCupCode: '',
+      imgUpLoadType: '未选择'
+    })
+  },
+
+
+  onClickUploadImage(e) {
+    let that = this
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#00CC00",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+
+    })
+  },
+
+  chooseWxImage(type) {
+    let that = this
+    wx.chooseImage({
+      count: 9, // 最多可以选择的图片张数，默认9
+      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sourceType: [type], // album 从相册选图，camera 使用相机，默认二者都有
+      success: function (res) {
+        // success
+        console.log(res)
+
+        that.setData({
+          imgUpLoadType: '假装已上传'
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+
+
+  onClickQuestionIcon(e) {
+    Toast({
+      duration: 1000,
+      message: '奶茶杯内杯壁上贴有编号',
+      selector: '#code-info'
+    })
+  },
+
+
+  //编号反向绑定
+
+  updateTeaCupCode(e) {
+    console.log(e.detail)
+    this.setData({
+      teaCupCode: e.detail
+    })
   }
 })
--- miniprogram/pages/teacup/teacup.js
+++ miniprogram/pages/teacup/teacup.js
@@ -1,18 +1,21 @@
 // miniprogram/pages/teacup/teacup.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    logged: app.logged
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      logged: app.logged
+    });
   },
 
   /**
