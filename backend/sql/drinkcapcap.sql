6c5ef3c55ed8457309e72506a92eac447bc5d2e0
Vaskka <1139851358@qq.com>
2019-03-21 18:28:29 +0800

    更改字符集编码错误

--- backend/sql/drinkcapcap.sql
+++ backend/sql/drinkcapcap.sql
@@ -196,7 +196,7 @@ CREATE TABLE `drink_point` (
   `shop_id` int(11) NOT NULL,
   `point` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -297,4 +297,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-21 18:19:49
+-- Dump completed on 2019-03-21 18:23:35
