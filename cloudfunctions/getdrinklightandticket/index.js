c411828f17830918ecd307093067c035e8a133fd
Vaskka <1139851358@qq.com>
2019-03-21 18:52:11 +0800

    更新接口，修复奶茶券控制表bug

--- cloudfunctions/getdrinklightandticket/index.js
+++ cloudfunctions/getdrinklightandticket/index.js
@@ -24,7 +24,68 @@ exports.main = async (event, context) => {
     tickets: []
   };
 
-  let pro_resp_ = got
+  // 获取全部shop信息
+  let resp = await got('http://129.204.216.249:8080/shop/all', {
+    method: 'GET',
+    headers: {
+      accept: "*/*"
+    },
+  });
+
+  let all_shops = JSON.parse(resp.body).data;
+
+
+  // 并发查找相应数据
+  let light_list = [];
+
+  // 奶茶积分
+  let pro_resp_point = got('http://129.204.216.249:8080/drinkpoint/get/' + wxContext.OPENID, {
+    method: 'GET',
+    headers: {
+      accept: "*/*"
+    },
+
+  }).then(function (res) {
+
+    let data = JSON.parse(res.body);
+
+    for (let obj of data) {
+      let _shop_id = obj.shop_id;
+
+      let model_shop = null;
+      for (let _shop of shop_list) {
+        if (_shop.shop_id == _shop_id) {
+          model_shop = _shop;
+          break;
+        }
+      }
+
+      // 存储结果
+      light_list.push({
+        number: obj.point,
+        shop: model_shop
+      });
+    }
+
+  });
+
+  let pro_resp_ticket = got('http://129.204.216.249:8080/drinkpoint/get/' + wxContext.OPENID, {
+    method: 'GET',
+    headers: {
+      accept: "*/*"
+    },
+
+  }).then(function (res) {
+
+    let data = JSON.parse(res.body);
+
+    for (let obj of data) {
+      
+
+    
+    }
+
+  });
   
   return result;
   
